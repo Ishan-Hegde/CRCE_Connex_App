7bd858a05b3a9b8e965914ae4fa20b8c03da087a
IshanFelix <hegdeishan10@gmail.com>
2024-03-22 23:19:13 +0530

    feedback working

--- lib/screens/feedback_section/view.dart
+++ lib/screens/feedback_section/view.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class FeedbackPage extends StatelessWidget {
@@ -9,108 +6,47 @@ class FeedbackPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const FeedbackWidget(),
-    );
-  }
-}
-
-class FeedbackWidget extends StatefulWidget {
-  const FeedbackWidget({Key? key});
-
-  @override
-  _FeedbackWidgetState createState() => _FeedbackWidgetState();
-}
-
-class _FeedbackWidgetState extends State<FeedbackWidget> {
-  final TextEditingController _feedbackController = TextEditingController();
-  final TextEditingController _replyController = TextEditingController();
-  bool isTeacher = true; // Set to false for student view
-
-  List<String> feedbackList = [
-    'Great work!',
-    'Could use some improvements.',
-    'Very informative.',
-  ];
-
-  List<String> replyList = [
-    'Thank you!',
-    'Noted, we will work on it.',
-    'Glad you found it useful!',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          if (!isTeacher)
-            TextField(
-              controller: _feedbackController,
-              decoration: const InputDecoration(
-                labelText: 'Feedback (for students)',
-                border: OutlineInputBorder(),
-              ),
-              maxLines: 5,
-            ),
-          if (!isTeacher) const SizedBox(height: 16.0),
-          if (!isTeacher)
-            ElevatedButton(
-              onPressed: () {
-                // Handle feedback submission
-                String feedback = _feedbackController.text.trim();
-                if (feedback.isNotEmpty) {
-                  // Process feedback submission (e.g., send to backend)
-                  // You can add your logic here
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Feedback submitted!')),
-                  );
-                  _feedbackController.clear();
-                  setState(() {
-                    feedbackList.add(feedback);
-                    replyList.add(''); // Initialize with an empty reply
-                  });
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please enter feedback.')),
-                  );
-                }
-              },
-              child: const Text('Submit Feedback'),
-            ),
-          const SizedBox(height: 32.0),
-          if (isTeacher)
-            Expanded(
-              child: ListView.builder(
-                itemCount: feedbackList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(feedbackList[index]),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 8),
-                        Text('Reply: ${replyList[index]}'),
-                        const Divider(),
-                      ],
-                    ),
-                    trailing: ElevatedButton(
-                      onPressed: () {
-                        _showReplyDialog(context, index);
-                      },
-                      child: const Text('Reply'),
-                    ),
-                  );
-                },
-              ),
-            ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: FeedbackModule(),
       ),
     );
   }
+}
 
-  void _showReplyDialog(BuildContext context, int index) {
+class FeedbackModule extends StatefulWidget {
+  const FeedbackModule({Key? key});
+
+  @override
+  _FeedbackModuleState createState() => _FeedbackModuleState();
+}
+
+class _FeedbackModuleState extends State<FeedbackModule> {
+  final TextEditingController _feedbackController = TextEditingController();
+  final TextEditingController _replyController = TextEditingController();
+
+  List<String> feedbackList = [];
+  List<String> replyList = [];
+
+  void submitFeedback() {
+    String feedback = _feedbackController.text.trim();
+    if (feedback.isNotEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Feedback submitted!')),
+      );
+      _feedbackController.clear();
+      setState(() {
+        feedbackList.add(feedback);
+        replyList.add(''); // Initialize with an empty reply
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please enter feedback.')),
+      );
+    }
+  }
+
+  void showReplyDialog(int index) {
     showDialog(
       context: context,
       builder: (context) {
@@ -149,6 +85,119 @@ class _FeedbackWidgetState extends State<FeedbackWidget> {
           ],
         );
       },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            // Show input feedback module
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Input Feedback'),
+                  content: TextField(
+                    controller: _feedbackController,
+                    decoration: const InputDecoration(
+                      labelText: 'Enter your feedback',
+                      border: OutlineInputBorder(),
+                    ),
+                    maxLines: 5,
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        submitFeedback();
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Submit Feedback'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xFFE57575),
+            textStyle: const TextStyle(fontSize: 18),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: const Text(
+            'Leave Feedback',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(height: 24.0),
+        ElevatedButton(
+          onPressed: () {
+            // Show previously sent messages module
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Feedback History'),
+                  content: SizedBox(
+                    height: 300,
+                    child: ListView.builder(
+                      itemCount: feedbackList.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            feedbackList[index],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text('Reply: ${replyList[index]}'),
+                        );
+                      },
+                    ),
+                  ),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(primary: Color(0xFFE57575)),
+                      child: const Text('Close'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xFFE57575),
+            textStyle: const TextStyle(fontSize: 18),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: const Text(
+            'View Feedback History',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
